7edfb5cc9586d72aba3ef610ff2b67ab326a9e32
andreacgv2000 <andreacgv2000@gmail.com>
2025-11-21 15:35:36 -0600

    index 7

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -224,3 +224,9 @@ if (document.getElementById('spotlights')) {
 }
 
 
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("show-menu");
+});
